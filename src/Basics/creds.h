b921504038e9903f53ce7b4f329f556ee4685c8f
vtmattedi <vtmattedi@gmail.com>
2025-10-01 21:21:29 -0300

    Added creds file exemple

---
+++ src/Basics/creds.h
@@ -0,0 +1,11 @@
+#define REMOTE_MQTT_URL "<your_remote_mqtt_url>"
+#define REMOTE_MQTT_PORT remoteport usually 8883 for TLS
+#define LOCAL_MQTT_HOST "<your_local_mqtt_ip>"
+#define LOCAL_MQTT_PORT localport usually 1883 for non-TLS
+#define MQTT_USER "yourusername"
+#define MQTT_PASSWD "yourpassword"
+#define MQTT_CREDS_H
+
+static const char *root_ca PROGMEM = R"EOF(
+remote mqtt broker root ca certificate here
+)EOF";
